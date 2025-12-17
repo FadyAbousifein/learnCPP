3c92fc32c8ddb3d2a458d0fe3e286631b13e6a54
Fady Abousifein <fadyasifein@gmail.com>
2025-12-17 10:23:43 -0500

    linked list implementation for ints

--- DSA/linkedlist/linked_list.cpp
+++ DSA/linkedlist/linked_list.cpp
@@ -1,39 +1,67 @@
+#include <cstdlib>
 #include <iostream> 
 
-struct node {
-    int data; 
-    node* next; 
+struct Node {
+    int data {};  
+    Node* next {};  
 }; 
 
-node* create_list() {
-    std::cout << "What size of list do you want: "; 
-    int size {}; 
-    std::cin >> size; 
+Node* create_list() {
+    std::cout << "How many nodes do you want in your list: "; 
+    int nodes {};
+    std::cin >> nodes; 
 
-    std::cout << "Enter your first value: "; 
+    Node* head {new Node}; 
+    Node* temp {head}; 
+
+    std::cout << "Enter first value: "; 
     int value {}; 
     std::cin >> value; 
-    size--; 
+    
+    head->data = value; 
 
-    node* head {new node {value, nullptr}};
-    node* current = head; 
-    for (int i = 0; i < size; i++) {
-        std::cout << "Enter the next value: "; 
-        std::cin >>  value; 
-        current->next = new node {value, nullptr};
-        current=current->next; 
+    for (int i = 0; i < nodes - 1; i++) {
+        std::cout << "Enter next value: "; 
+        std::cin >> value; 
+        temp->next =  new Node; 
+        temp->next->data = value; 
+        temp = temp->next; 
     }
-    return head;
+
+    return head; 
 }
 
-void print_list(const node* head) {
-    while(head != nullptr) {
-        std::cout << head->data << " -> ";
+void print_list(const Node* head) {
+    while (head != nullptr) {
+        std::cout << head->data << " -> "; 
         head = head->next; 
-    } std::cout << std::endl;
+    }std::cout << std::endl; 
+}
+
+void insert_node(Node* head, Node& node, int position) {
+    for (int i = 0; i < position - 2; i++) {
+        head = head->next;  
+    } 
+    node.next = head->next; 
+    head->next = &node; 
+}
+
+// fix for position 1 and delete the actual node bypassed
+void delete_node(Node* head, int position) {
+    for(int i = 0; i < position - 2; i++) {
+        head = head->next; 
+    }
+    head->next = head->next->next; 
 }
 
 int main (int argc, char *argv[]) {
-    print_list(create_list());  
+    Node* head = create_list(); 
+    print_list(head); 
+    Node node {90}; 
+    insert_node(head, node, 3);  
+    print_list(head); 
+    delete_node(head, 1); 
+    print_list(head); 
+
     return 0;
 }
